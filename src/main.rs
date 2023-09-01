f7fe7c73401892d5c8f00f851341b5ab8be5e55d
Grant <grant@Grants-MacBook-Pro.local>
2023-09-01 16:44:01 -0500

    good
    gooder

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     print_prompt_with_number();
 }
 
-fn give_hint(number: i16, number_to_guess: i16) {
+fn give_hint(number: i32, number_to_guess: i32) {
     if number < number_to_guess {
         println!("Your guess is too low!");
     } else if number > number_to_guess {
@@ -21,6 +21,11 @@ fn valid(number_one: i32, number_two: i32) -> bool {
 /*
 FIXME: split this method into multiple methods
 */
+fn generate_random_num() -> i32 {
+    let mut rng = rand::thread_rng();
+    let number = rng.gen_range(10..250);
+    return number;
+}
 fn print_prompt_with_number() {
     let mut rng = rand::thread_rng();
     let mut start: i32 = rng.gen_range(10..250);
@@ -34,14 +39,16 @@ fn print_prompt_with_number() {
         start = rng.gen_range(10..250)
     }
     let mut simulated_random_num: i32 = rng.gen_range(10..250);
-    let mut count = 1;
+    let mut count = 0;
     while simulated_random_num != number_to_guess {
+        count += 1;
         simulated_random_num = rng.gen_range(10..250);
+        give_hint(simulated_random_num, number_to_guess);
         println!("Guessing again...");
+        println!("The number guessed was {}", simulated_random_num);
         if simulated_random_num == number_to_guess {
             println!("Correctly guessed {} in {} tries", number_to_guess, count)
         }
-        count += 1;
     }
     if !valid(start, end) {
         println!("These numbers are not within the constraints of the rubric... re-rolling");
