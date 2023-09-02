dffa2874e4d24ff06bc75b7d2788e6e184d8e707
hrantgartley <doyouevenjump@gmail.com>
2023-09-01 19:37:27 -0500

    refactor
    added a validate_bounds func. and a function to simulate user guess => (could have just used the generate number method but i did it for the debugging process)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#![allow(dead_code)]
 use rand::prelude::*;
 
 fn main() {
@@ -12,10 +11,6 @@ fn give_hint(number: i32, number_to_guess: i32) {
     }
 }
 
-fn valid(number_one: i32, number_two: i32) -> bool {
-    number_one < 1000 && number_one > 0 && number_two < 1000 && number_two > 0
-}
-
 /*
 FIXME: split this method into multiple methods
 */
@@ -26,21 +21,17 @@ fn generate_random_num() -> i32 {
 
 fn print_prompt_with_number() {
     let mut rng = rand::thread_rng();
-    let mut start: i32 = generate_random_num();
-    let mut end: i32 = generate_random_num();
-    let number_to_guess: i32 = generate_random_num();
-    if number_to_guess > end {
-        println!("{} is bigger than {}... re-rolling", number_to_guess, end);
-        end = rng.gen_range(10..250)
-    } else if number_to_guess < start {
-        println!("{} is bigger than {}... re-rolling", number_to_guess, start);
-        start = rng.gen_range(10..250)
-    }
-    let mut simulated_random_num: i32 = rng.gen_range(10..250);
+    let mut start = generate_random_num();
+    let mut end = generate_random_num();
+    let number_to_guess = generate_random_num();
+    let mut simulated_random_num = rng.gen_range(10..250);
     let mut count = 0;
+
+    validate_bounds(&mut start, &mut end);
+
     while simulated_random_num != number_to_guess {
         count += 1;
-        simulated_random_num = rng.gen_range(10..250);
+        simulated_random_num = generate_simulated_random_num();
         give_hint(simulated_random_num, number_to_guess);
         println!("Guessing again...");
         println!("The number guessed was {}", simulated_random_num);
@@ -48,8 +39,25 @@ fn print_prompt_with_number() {
             println!("Correctly guessed {} in {} tries", number_to_guess, count)
         }
     }
-    if !valid(start, end) {
-        println!("These numbers are not within the constraints of the rubric... re-rolling");
-        print_prompt_with_number();
+}
+
+fn validate_bounds(start: &mut i32, end: &mut i32) {
+    let mut rng = rand::thread_rng();
+    let number_to_guess = generate_random_num();
+
+    if number_to_guess > *end {
+        println!("{} is bigger than {}... re-rolling", number_to_guess, *end);
+        *end = rng.gen_range(10..250)
+    } else if number_to_guess < *start {
+        println!(
+            "{} is bigger than {}... re-rolling",
+            number_to_guess, *start
+        );
+        *start = rng.gen_range(10..250)
     }
+}
+
+fn generate_simulated_random_num() -> i32 {
+    let mut rng = rand::thread_rng();
+    rng.gen_range(10..250)
 }
