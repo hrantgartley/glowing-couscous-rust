51691fe4fd94259a761f641bc9c0497db866bdac
Grant <grant@Grants-MacBook-Pro.local>
2023-09-03 19:17:41 -0500

    Refactored
    let users input guesses instead of simulating the guessing and also added a play_game() method that allows for simpler main

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use rand::prelude::*;
 
 fn main() {
     welcome_message();
-    print_prompt_with_number()
+    let number_to_guess = generate_random_num();
+    play_game(number_to_guess);
 }
+
 fn give_hint(number: i32, number_to_guess: i32) {
     match number.cmp(&number_to_guess) {
         std::cmp::Ordering::Less => println!("Your guess is too low!"),
@@ -11,20 +13,38 @@ fn give_hint(number: i32, number_to_guess: i32) {
         std::cmp::Ordering::Equal => println!("You have guessed the number correctly"),
     }
 }
-/*
-FIXME: error that incorrectly calculates the number of attempts it takes to guess a number
-FIXME: also at the end it shows reroll prompt
-*/
-fn loop_until_done(mut sim: i32, num: i32) {
-    while sim != num {
-        let mut count = 1;
+
+fn play_game(number_to_guess: i32) {
+    let mut count = 0;
+
+    loop {
+        let user_guess = get_user_guess();
         count += 1;
-        sim = generate_simulated_random_num();
-        give_hint(sim, num);
-        println!("Guessing again...");
-        println!("The number guessed was {}", sim);
-        if sim == num {
-            println!("Correctly guessed {} in {} tries", num, count)
+
+        give_hint(user_guess, number_to_guess);
+
+        if user_guess == number_to_guess {
+            println!("Correctly guessed {} in {} tries", number_to_guess, count);
+            break;
+        }
+    }
+}
+
+fn get_user_guess() -> i32 {
+    loop {
+        println!("Enter your guess:");
+        let mut guess = String::new();
+
+        std::io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        match guess.trim().parse() {
+            Ok(num) => return num,
+            Err(_) => {
+                println!("Please enter a valid number.");
+                continue;
+            }
         }
     }
 }
@@ -34,38 +54,7 @@ fn generate_random_num() -> i32 {
     rng.gen_range(10..250)
 }
 
-fn print_prompt_with_number() {
-    let mut rng = rand::thread_rng();
-    let mut start = generate_random_num();
-    let mut end = generate_random_num();
-    let number_to_guess = generate_random_num();
-    let simulated_random_num = rng.gen_range(10..250);
-    loop_until_done(simulated_random_num, number_to_guess);
-    validate_bounds(&mut start, &mut end);
-}
-
-fn validate_bounds(start: &mut i32, end: &mut i32) {
-    let mut rng = rand::thread_rng();
-    let number_to_guess = generate_random_num();
-
-    if number_to_guess > *end {
-        println!("{} is bigger than {}... re-rolling", number_to_guess, *end);
-        *end = rng.gen_range(10..250)
-    } else if number_to_guess < *start {
-        println!(
-            "{} is bigger than {}... re-rolling",
-            number_to_guess, *start
-        );
-        *start = rng.gen_range(10..250)
-    }
-}
-
-fn generate_simulated_random_num() -> i32 {
-    let mut rng = rand::thread_rng();
-    rng.gen_range(10..250)
-}
-
-fn welcome_message() -> () {
+fn welcome_message() {
     println!("Welcome to the guessing game...");
     println!("---------------------------------");
 }
